91dcc6fc9c5ecd66e6109ba663ae04a9a6ef2f6b
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2024-08-23 17:43:23 -0700

    aaa

--- mongodb.py
+++ mongodb.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://dlujan226:Daniel2006_@cluster0.57atq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+uri = "API-KEY"
+client = MongoClient(uri, server_api=pymongo.server_api.ServerA)
 db = client['db1']
 collection = db['youtube']
 
@@ -8,4 +10,3 @@ insert_doc = collection.insert_one(document)
 
 print(f"inserted Document ID : {insert_doc.inserted_id}")   
 client.close()
-
